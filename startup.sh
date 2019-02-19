3d62241e4303c23d1aba2c4aa71da213b726cd84
fineyjt <13261900961@163.com>
2019-02-19 17:13:56 +0800

    just commit

--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ if [ -f "$IMPULSE_PID" ] && kill -0 $(cat "$IMPULSE_PID"); then
 	rm -rf "$IMPULSE_PID"
 	echo "=========进程kill完成,准备启动==========="
 fi
-nohup java -jar -Xms200m -Xmx256m  impulse/target/impulse-0.0.1.jar --spring.profiles.active=prod>> logs/nohup.out 2>&1 & echo $! > $IMPULSE_PID
+nohup java -jar -Xms200m -Xmx256m  impulse/target/impulse-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod>> logs/nohup.out 2>&1 & echo $! > $IMPULSE_PID
 echo "=========启动完成========="
